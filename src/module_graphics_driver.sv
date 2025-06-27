14e0e9f881f15917efeeb83429050cffec6107b4
a <yonatan02greental@gmail.com>
2025-06-27 10:01:28 +0000

    Removed the state machine for the display as it is pretty boilerplate. Replaced with a counters only approach which improved timing

--- src/module_graphics_driver.sv
+++ src/module_graphics_driver.sv
@@ -1,13 +1,13 @@
 module graphics_driver #(
-    parameter int HEIGHT_COUNTER_SIZE           ,
-    parameter int WIDTH_COUNTER_SIZE            ,
-    parameter int INITIAL_PADDLE_1_X            ,
-    parameter int INITIAL_PADDLE_2_X            ,
-    parameter int INITIAL_PADDLE_Y              ,
-    parameter int INITIAL_BALL_X                ,
-    parameter int INITIAL_BALL_Y                ,
-    parameter int PADDLE_WIDTH                  ,
-    parameter int PADDLE_HEIGHT                 ,
+    parameter int HEIGHT_COUNTER_SIZE,
+    parameter int WIDTH_COUNTER_SIZE,
+    parameter int INITIAL_PADDLE_1_X,
+    parameter int INITIAL_PADDLE_2_X,
+    parameter int INITIAL_PADDLE_Y,
+    parameter int INITIAL_BALL_X,
+    parameter int INITIAL_BALL_Y,
+    parameter int PADDLE_WIDTH,
+    parameter int PADDLE_HEIGHT,
     parameter int BALL_SIDE_SIZE
 ) (
     input clk,  // This is a proper ~25.172 MHz clock.
@@ -23,13 +23,6 @@ module graphics_driver #(
     output logic green_s,
     output logic blue_s
 );
-
-  typedef enum logic [1:0] {
-    COORDINATE_ACTIVE = 2'b00,
-    COORDINATE_FRONT  = 2'b01,
-    COORDINATE_PULSE  = 2'b10,
-    COORDINATE_BACK   = 2'b11
-  } screen_state_t;
 
   localparam int X_ACTIVE_IN_CLOCKS = 640;
   localparam int X_FRONT_IN_CLOCKS = 16;
@@ -61,11 +54,6 @@ module graphics_driver #(
   logic [WIDTH_COUNTER_SIZE:0] next_ball_pos_x;
   logic [HEIGHT_COUNTER_SIZE:0] next_ball_pos_y;
 
-  screen_state_t x_state;
-  screen_state_t y_state;
-  screen_state_t next_x_state;
-  screen_state_t next_y_state;
-
   logic x_line_finished;
 
   // Unsampled outputs - Used internally.
@@ -78,15 +66,12 @@ module graphics_driver #(
   // Graphics driver sampler.
   always_ff @(posedge clk or negedge rst)
     if (~rst) begin
-      x_state <= COORDINATE_ACTIVE;
-      y_state <= COORDINATE_ACTIVE;
       x_change_counter <= '0;
       y_change_counter <= '0;
-      // We zero these. after reset both counters are zero and states are active so we dont refresh the correct positions for a single frame.
       sampled_paddle_1_pos <= INITIAL_PADDLE_Y;
       sampled_paddle_2_pos <= INITIAL_PADDLE_Y;
-      sampled_ball_pos_x   <= INITIAL_BALL_X;
-      sampled_ball_pos_y   <= INITIAL_BALL_Y;
+      sampled_ball_pos_x <= INITIAL_BALL_X;
+      sampled_ball_pos_y <= INITIAL_BALL_Y;
     end else begin
       // Sample the object positions.
       sampled_paddle_1_pos <= next_paddle_1_pos;
@@ -96,96 +81,49 @@ module graphics_driver #(
       // Sample x and y counters.
       x_change_counter <= next_x_change_counter;
       y_change_counter <= next_y_change_counter;
-      // Sample the x and y states.
-      x_state <= next_x_state;
-      y_state <= next_y_state;
     end
 
   // Horizontal and vertical sync logic.
   always_comb begin
-    // Default values.
+    // Default assignments.
     next_paddle_1_pos = sampled_paddle_1_pos;
     next_paddle_2_pos = sampled_paddle_2_pos;
     next_ball_pos_x = sampled_ball_pos_x;
     next_ball_pos_y = sampled_ball_pos_y;
+
     next_x_change_counter = x_change_counter + 1;
-    next_y_change_counter = y_change_counter;
-    next_x_state = x_state;
-    next_y_state = y_state;
-    hsync = 1'b1;
-    vsync = 1'b1;
     x_line_finished = 1'b0;
 
-    unique case (x_state)
-      COORDINATE_ACTIVE: begin
-        hsync = 1'b1;
-        if (next_x_change_counter == X_ACTIVE_IN_CLOCKS) 
-          next_x_state = COORDINATE_FRONT;
-      end
-      COORDINATE_FRONT: begin
-        hsync = 1'b1;
-        if (next_x_change_counter == X_ACTIVE_IN_CLOCKS + X_FRONT_IN_CLOCKS)
-          next_x_state = COORDINATE_PULSE;
-      end
-      COORDINATE_PULSE: begin
-        hsync = 1'b0;
-        if (next_x_change_counter == X_ACTIVE_IN_CLOCKS + X_FRONT_IN_CLOCKS + X_PULSE_IN_CLOCKS)
-          next_x_state = COORDINATE_BACK;
-      end
-      COORDINATE_BACK: begin
-        hsync = 1'b1;
-        if (next_x_change_counter == X_ACTIVE_IN_CLOCKS + X_FRONT_IN_CLOCKS + X_PULSE_IN_CLOCKS + X_BACK_IN_CLOCKS) begin
-          next_x_state = COORDINATE_ACTIVE;
-          next_x_change_counter = '0;
-          x_line_finished = 1'b1;
-        end
-      end
-      default: begin
-        // This is impossible.
-      end
-    endcase
+    // Reset x counter and trigger y counter increment.
+    if (next_x_change_counter == TOTAL_X) begin
+      next_x_change_counter = '0;
+      x_line_finished = 1'b1;
+    end
 
     next_y_change_counter = y_change_counter + x_line_finished;
 
-    unique case (y_state)
-      COORDINATE_ACTIVE: begin
-        vsync = 1'b1;
-        if (next_y_change_counter == Y_ACTIVE_IN_LINES) next_y_state = COORDINATE_FRONT;
-      end
-      COORDINATE_FRONT: begin
-        vsync = 1'b1;
-        if (next_y_change_counter == Y_ACTIVE_IN_LINES + Y_FRONT_IN_LINES)
-          next_y_state = COORDINATE_PULSE;
-      end
-      COORDINATE_PULSE: begin
-        vsync = 1'b0;
-        if (next_y_change_counter == Y_ACTIVE_IN_LINES + Y_FRONT_IN_LINES + Y_PULSE_IN_LINES)
-          next_y_state = COORDINATE_BACK;
-      end
-      COORDINATE_BACK: begin
-        vsync = 1'b1;
-        if (next_y_change_counter == Y_ACTIVE_IN_LINES + Y_FRONT_IN_LINES + Y_PULSE_IN_LINES + Y_BACK_IN_LINES) begin
-          next_y_state = COORDINATE_ACTIVE;
-          next_y_change_counter = '0;
-          // In this case, we need to re-sample the game shape positions.
-          next_paddle_1_pos = paddle_1_pos;
-          next_paddle_2_pos = paddle_2_pos;
-          next_ball_pos_x = ball_pos_x;
-          next_ball_pos_y = ball_pos_y;
-        end
-      end
-      default: begin
-        // This is impossible.
-      end
-    endcase
+    hsync = ((x_change_counter >= (X_ACTIVE_IN_CLOCKS + X_FRONT_IN_CLOCKS)) &&
+           (x_change_counter <  (X_ACTIVE_IN_CLOCKS + X_FRONT_IN_CLOCKS + X_PULSE_IN_CLOCKS))) ? 1'b0 : 1'b1;
 
+    vsync = ((y_change_counter >= (Y_ACTIVE_IN_LINES + Y_FRONT_IN_LINES)) &&
+           (y_change_counter <  (Y_ACTIVE_IN_LINES + Y_FRONT_IN_LINES + Y_PULSE_IN_LINES))) ? 1'b0 : 1'b1;
+
+    // Reset vertical counter and resample game positions.
+    if (next_y_change_counter == TOTAL_Y) begin
+      next_y_change_counter = '0;
+
+      next_paddle_1_pos = paddle_1_pos;
+      next_paddle_2_pos = paddle_2_pos;
+      next_ball_pos_x = ball_pos_x;
+      next_ball_pos_y = ball_pos_y;
+    end
   end
 
   // Pixel color logic.
   always_comb begin
     {red, green, blue} = 3'b000;
     // If x and y inside any of the shapes - display (R G B = 1) else display (R G B = 0).
-    if ((x_state == COORDINATE_ACTIVE) && (y_state == COORDINATE_ACTIVE)) begin
+    if ((x_change_counter < X_ACTIVE_IN_CLOCKS) && (y_change_counter < Y_ACTIVE_IN_LINES)) begin
       if ((x_change_counter >= INITIAL_PADDLE_1_X) && (x_change_counter < INITIAL_PADDLE_1_X + PADDLE_WIDTH) && (y_change_counter >= sampled_paddle_1_pos) && (y_change_counter < sampled_paddle_1_pos + PADDLE_HEIGHT)) begin
         {red, green, blue} = 3'b111;
       end
